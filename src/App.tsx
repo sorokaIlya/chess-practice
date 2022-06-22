fdbf42fc28819abc17da56ef11b642f3a4412b25
ilasorokin <revolut_mozart@icloud.com>
2022-06-22 13:08:03 +0300

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const refComponent = useRef<boolean>();
+    useEffect(() => {
+        refComponent.current = false
+        refComponent.current && console.count('render');
+        return () => {
+            refComponent.current = true
+        }
+    }, [])
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
