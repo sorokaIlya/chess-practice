244b743f5359d72301b407b00eaf577e3b89c1da
ilasorokin <revolut_mozart@icloud.com>
2022-06-23 21:31:59 +0300

    add-components

---
+++ src/models/ChessEnum.ts
@@ -0,0 +1,18 @@
+export enum ChessEnum {
+    WHITE = "white",
+    BLACK = "black"
+}
+export enum GameEnum{
+    BLACK_WIN = 'black_win',
+    WHITE_WIN = 'white_win',
+    RESTART = 'restart'
+}
+
+export enum FigureType{
+    BISHOP,
+    KING,
+    ROOK,
+    HORSE,
+    PAWN,
+    
+}
